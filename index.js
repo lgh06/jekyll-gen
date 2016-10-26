9b0ed4d476c61295e5da308aab79d3715cbe9db1
lgh06 <liugehuan@kankan.com>
2016-10-26 12:52:55 +0800

    add -t option to modify title

--- index.js
+++ index.js
@@ -8,31 +8,27 @@ var program = require('commander');
 
 program
   .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .option('-t, --title [title]', 'Add title,default is 来吧 来吧 相约16 ', '来吧 来吧 相约16')
   .parse(process.argv);
 
 
 const fs = require('fs');
 const tpl = require('./template');
-const now = require('./now');
+const now = require('./now')();
 
-const data = tpl('来吧 来吧',now());
+const title = program.title || '来吧 来吧 相约98'; //if no -t option,default title will be '来吧 来吧 相约98'
+
+const data = tpl(title,now);
 console.log(data)
 
 
 
 fs.writeFile('message.txt', data, (err) => {
   if (err) throw err;
-  console.log('It\'s saved!');
+  console.log('File is saved!');
 });
 
 
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+
+console.log(' title is - %s ', title);
